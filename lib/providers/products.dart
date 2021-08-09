863cfa0f3da29ecec4f8dca1adc376fc44930d86
Gabriel Albuquerque da Rosa <gabaoalb@gmail.com>
2021-08-09 19:26:31 -0300

    database one

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import 'product.dart';
 
@@ -83,15 +86,25 @@ class Products with ChangeNotifier {
   // }
 
   void addProduct(Product product) {
-    final newProduct = Product(
-        id: DateTime.now().toString(),
-        title: product.title,
-        description: product.description,
-        price: product.price,
-        imageUrl: product.imageUrl
-    );
-    _items.add(newProduct);
-    notifyListeners();
+    final url = Uri.https('flutter-course-f37f6-default-rtdb.firebaseio.com', '/products.json');
+    http.post(url, body: json.encode({
+      'title': product.title,
+      'description': product.description,
+      'price': product.price,
+      'imageUrl': product.imageUrl,
+      'isFavorite': product.isFavorite,
+    })).then((response) {
+      print(json.decode(response.body));
+      final newProduct = Product(
+          id: json.decode(response.body)['name'],
+          title: product.title,
+          description: product.description,
+          price: product.price,
+          imageUrl: product.imageUrl
+      );
+      _items.add(newProduct);
+      notifyListeners();
+    });
   }
 
   void editProduct(String id, Product editedProduct) {
