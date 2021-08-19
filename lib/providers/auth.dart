dc6a48dc0ef0554c0a14d85964aa51b245ef9785
Gabriel Albuquerque da Rosa <gabriel.rosa@effectiveerp.net>
2021-08-19 13:32:18 -0300

    autologin error handling

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -23,7 +23,9 @@ class Auth with ChangeNotifier {
   }
 
   String? get token {
-    if (_expiryDate != null && _expiryDate!.isAfter(DateTime.now()) && _token != null) {
+    if (_expiryDate != null &&
+        _expiryDate!.isAfter(DateTime.now()) &&
+        _token != null) {
       //print('teste datetime deu after');
       return _token;
     }
@@ -34,9 +36,11 @@ class Auth with ChangeNotifier {
     return _userId;
   }
 
-  Future<void> _authenticate(String? email, String? password, String urlSegment) async {
-    final url = Uri.parse('https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=$_key');
-    try{
+  Future<void> _authenticate(
+      String? email, String? password, String urlSegment) async {
+    final url = Uri.parse(
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=$_key');
+    try {
       final response = await http.post(url,
           body: json.encode({
             'email': email,
@@ -44,28 +48,27 @@ class Auth with ChangeNotifier {
             'returnSecureToken': true
           }));
       final responseData = json.decode(response.body);
-      if(responseData['error'] != null) {
+      if (responseData['error'] != null) {
         throw HttpException(responseData['error']['message']);
       }
       _token = responseData['idToken'];
       _userId = responseData['localId'];
-      _expiryDate = DateTime.now().add(Duration(seconds: int.parse(responseData?['expiresIn'])));
+      _expiryDate = DateTime.now()
+          .add(Duration(seconds: int.parse(responseData?['expiresIn'])));
       // print(responseData['expiresIn']);
       // print(_expiryDate);
-      //_autoLogout();
+      _autoLogout();
       notifyListeners();
       final prefs = await SharedPreferences.getInstance();
-      //prefs.setString(key, value);
       final userData = json.encode({
         'token': _token,
         'userId': _userId,
         'expiryDate': _expiryDate!.toIso8601String()
       });
       prefs.setString('userData', userData);
-    } catch(error) {
+    } catch (error) {
       throw error;
     }
-
   }
 
   Future<void> singUp(String? email, String? password) async {
@@ -77,32 +80,38 @@ class Auth with ChangeNotifier {
   }
 
   Future<bool> tryAutoLogin() async {
+    print('chamou');
     final prefs = await SharedPreferences.getInstance();
-    try{
-      if(!prefs.containsKey('userData')){
-        return false;
-      }
-      final extractedUserData = json.decode(prefs.getString('userData')!) as Map<String, Object>;
-      final expiryDate = DateTime.parse(extractedUserData['expiryDate'].toString());
-      if(expiryDate.isBefore(DateTime.now())){
-        return false;
-      }
-      _token = extractedUserData['token'].toString();
-      _userId = extractedUserData['userId'].toString();
-      _expiryDate = expiryDate;
-      notifyListeners();
-      _autoLogout();
-      return true;
-    } catch(error) {
-      throw error;
+    print('01');
+    if (!prefs.containsKey('userData')) {
+      print('false contains');
+      return false;
     }
+    print('02');
+    final extractedUserData = json.decode(prefs.getString('userData')!) as Map<String, Object>; //aqui tem problema!
+    print('03');
+    final expiryDate = DateTime.parse(extractedUserData['expiryDate'].toString());
+    print('04');
+    if (expiryDate.isBefore(DateTime.now())) {
+      print('false data');
+      return false;
+    }
+    print('05');
+    _token = extractedUserData['token'].toString();
+    _userId = extractedUserData['userId'].toString();
+    _expiryDate = expiryDate;
+    print('$_token');
+    print(_expiryDate);
+    notifyListeners();
+    _autoLogout();
+    return true;
   }
 
   Future<void> logout() async {
     _token = null;
     _userId = null;
     _expiryDate = null;
-    if(_authTimer != null){
+    if (_authTimer != null) {
       _authTimer!.cancel();
       _authTimer = null;
     }
@@ -112,7 +121,7 @@ class Auth with ChangeNotifier {
   }
 
   void _autoLogout() {
-    if(_authTimer != null){
+    if (_authTimer != null) {
       _authTimer!.cancel();
     }
     final timeToExpiry = _expiryDate!.difference(DateTime.now()).inSeconds;
